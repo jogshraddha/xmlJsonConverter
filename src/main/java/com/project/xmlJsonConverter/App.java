2d6cfe71172583e69103d596d6445a9eaa18094c
jogshraddha <jog.shraddha@gmail.com>
2017-05-15 15:41:10 +0530

    file name modification

--- src/main/java/com/project/xmlJsonConverter/App.java
+++ src/main/java/com/project/xmlJsonConverter/App.java
@@ -12,11 +12,15 @@ public class App {
 	public static void main(String[] args) {
 		final String dir = App.class.getClassLoader().getResource("ALPH-PROD-0413-1022").getPath();
 		File directory = new File(dir);
+		String extension = ".json";
+		JsonWriter jsonWriter = new JsonWriter();
 		File[] list = directory.listFiles();
 		for (File file : list) {
 			String jsonString = new ConversionUtil().convertToJson(file);
-			File jsonFile = new File(dir + File.separator + file.getName() + ".json");
-			new JsonWriter().writeJson(jsonFile, jsonString);
+			StringBuilder jsonPathBuilder = new StringBuilder();
+			jsonPathBuilder.append(dir).append(File.separator).append(file.getName().split("\\.")[0]).append(extension);
+			File jsonFile = new File(jsonPathBuilder.toString());
+			jsonWriter.writeJson(jsonFile, jsonString);
 		}
 		
 	}
